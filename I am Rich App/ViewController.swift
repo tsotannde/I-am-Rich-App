dd15d0802a17d30b7d0fa3e9f7745c001840b737
Adebayo Sotannde <me@adebayosotannde.com>
2024-02-01 18:22:11 -0500

    Initial Commit. Hello World

---
+++ I am Rich App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich App
+//
+//  Created by Adebayo Sotannde on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
